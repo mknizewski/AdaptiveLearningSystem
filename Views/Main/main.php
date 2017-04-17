5adc1445a04bd5bd7e4d86eafa816193475b0adb
mknizewski <mateusz.knizewski@gmail.com>
2017-04-17 15:30:30 +0200

    #mk - wstęp do main page

--- Views/Main/main.php
+++ Views/Main/main.php
@@ -1,3 +1,41 @@
-<?php
-    echo "elo";
-?>
+<div class="row">
+    <div class="col-md-8">
+    <img class="img-responsive img-rounded" src="Content/images/main.jpeg" alt="" width="900">
+</div>
+
+<div class="col-md-4">
+        <h1>Adaptacyjny system e-learningowy</h1>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+        <a class="btn btn-primary btn-lg" href="index.php?con=3&page=2">Zarejestruj się już teraz!</a>
+    </div>
+</div>
+ 
+<hr>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="well text-center">
+            Tu jakieś motto życiowe serwisu czy jakaś inna pierdoła!
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-4">
+        <h2>Cecha 1</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+        <a class="btn btn-default" href="#">More Info</a>
+    </div>
+    
+    <div class="col-md-4">
+        <h2>Cecha 2</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+        <a class="btn btn-default" href="#">More Info</a>
+    </div>
+    
+    <div class="col-md-4">
+        <h2>Cecha 3</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+        <a class="btn btn-default" href="#">More Info</a>
+    </div>
+</div>
