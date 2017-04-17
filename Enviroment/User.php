c4e8f4de6edb4dfedb1556aa86c03186a7f6883e
mknizewski <mateusz.knizewski@gmail.com>
2017-04-17 13:38:22 +0200

    #mk - metoda zwracajaca rolę usera

--- Enviroment/User.php
+++ Enviroment/User.php
@@ -1,4 +1,6 @@
 <?php
+    include_once 'Enviroment/DbContext.php';
+
     class User
     {
         public $Name;
@@ -8,7 +10,25 @@
 
         public function GetRole()
         {
-            
+            $role = "";
+            $dbContext = new DbContext();
+            $statment = $this -> RoleStatement();
+
+            $result = $dbContext -> Select($statment);
+
+            if ($dbResult -> num_rows > 0)
+            {
+                $row = $dbResult -> fetch_assoc();
+                $role = $row["name"];
+            }
+
+            return $role;
+        }
+
+        private function RoleStatement()
+        {
+            $roleStatment = "SELECT * FROM roles WHERE role_id = '$RoleId'";
+            return $roleStatment;
         }
     }
 ?>
