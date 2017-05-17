07d663af3c5b50f62c69445c2efe8b90fbb22c50
mknizewski <mateusz.knizewski@gmail.com>
2017-05-17 16:50:18 +0200

    #mk - poprawka do addmodule.php

--- Views/Admin/addmodule.php
+++ Views/Admin/addmodule.php
@@ -135,20 +135,7 @@
         </div>
     </div>
 </div>
-<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
 <script>
-	tinymce.init({ selector:'textarea',  
-	 plugins: [
-        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-        "searchreplace wordcount visualblocks visualchars code fullscreen",
-        "insertdatetime media nonbreaking save table contextmenu directionality",
-        "emoticons template paste textcolor autoresize"
-    ],
-    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-    toolbar2: "print preview media | forecolor backcolor emoticons",
-    image_advtab: true });
-
-
     function GetLessonByCourseId()
     {
         var courseId = document.getElementById('course').value;
